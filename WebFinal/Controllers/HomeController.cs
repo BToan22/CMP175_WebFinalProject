503e38da9ed47b64abeb95c48ba3d512071723d3
congdanh1437 <ddanh14372@gmail.com>
2023-01-09 00:03:55 +0700

    subject and student done

--- WebFinal/Controllers/HomeController.cs
+++ WebFinal/Controllers/HomeController.cs
@@ -73,6 +73,7 @@ namespace WebFinal.Controllers
                 return Json(res);
             }
         }
+       
         public IActionResult delete_course(int id)
         {
             var c = deleteCourse(id);
@@ -121,6 +122,128 @@ namespace WebFinal.Controllers
             }
 
         }
+        public IActionResult insert_course(Subject subject)
+        {
+            var c = insertCourse(subject);
+            if (c != null)
+            {
+                var res = new
+                {
+                    Success = true,
+                    Message = "",
+                    Data = c
+                };
+                return Json(res);
+            }
+            else
+            {
+                var res = new
+                {
+                    Success = false,
+                    Message = "ERROR",
+                };
+                return Json(res);
+            }
+
+        }
+        public IActionResult get_student(int Page, int Size)
+        {
+            var data = getStudent(Page, Size);
+            if (data != null)
+            {
+                var res = new
+                {
+                    Success = true,
+                    Message = "",
+                    Data = data
+                };
+                return Json(res);
+            }
+            else
+            {
+                var res = new
+                {
+                    Success = false,
+                    Message = "ERROR",
+                };
+                return Json(res);
+            }
+        }
+
+        public IActionResult delete_student(int id)
+        {
+            var c = deleteStudent(id);
+            if (c != null)
+            {
+                var res = new
+                {
+                    Success = c,
+                    Message = "",
+
+                };
+                return Json(res);
+            }
+            else
+            {
+                var res = new
+                {
+                    Success = false,
+                    Message = "ERROR",
+                };
+                return Json(res);
+            }
+        }
+        
+        public IActionResult insert_student(Student student)
+        {
+            var c = insertStudent(student);
+            if (c != null)
+            {
+                var res = new
+                {
+                    Success = true,
+                    Message = "",
+                    Data = c
+                };
+                return Json(res);
+            }
+            else
+            {
+                var res = new
+                {
+                    Success = false,
+                    Message = "ERROR",
+                };
+                return Json(res);
+            }
+
+        }
+        public IActionResult update_student(Student student)
+        {
+            var c = updateStudent(student);
+            if (c != null)
+            {
+                var res = new
+                {
+                    Success = true,
+                    Message = "",
+                    Data = c
+                };
+                return Json(res);
+            }
+            else
+            {
+                var res = new
+                {
+                    Success = false,
+                    Message = "ERROR",
+                };
+                return Json(res);
+            }
+
+        }
+
+        //PRIVATE
         private object? getCourse(string mj, int page, int size)
         {
             try
@@ -149,31 +272,7 @@ namespace WebFinal.Controllers
             }
 
         }
-
-        public IActionResult insert_course(Subject subject)
-        {
-            var c = insertCourse(subject);
-            if (c != null)
-            {
-                var res = new
-                {
-                    Success = true,
-                    Message = "",
-                    Data = c
-                };
-                return Json(res);
-            }
-            else
-            {
-                var res = new
-                {
-                    Success = false,
-                    Message = "ERROR",
-                };
-                return Json(res);
-            }
-
-        }
+        
         private Boolean? deleteCourse(int? id)
         {
             try
@@ -244,6 +343,121 @@ namespace WebFinal.Controllers
                 db.Subjects.Update(c1);
                 db.SaveChanges();
                 return c1;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        //STUDENT
+        
+        
+        private object? getStudent(int page, int size)
+        {
+            try
+            {
+                var db = new WebFinalContext();
+                var ls = db.Students.ToList();
+
+                var offset = (page - 1) * size;
+                var totalRecord = ls.Count();
+                var totalPage = (totalRecord % size) == 0 ?
+                    (int)(totalRecord / size) :
+                    (int)(totalRecord / size + 1);
+                var lst = ls.Skip(offset).Take(size).ToList();
+                return new
+                {
+                    Data = lst,
+                    TotalRecord = totalRecord,
+                    TotalPage = totalPage,
+                    Page = page,
+                    Size = size
+                };
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+        private Boolean? deleteStudent(int? id)
+        {
+            try
+            {
+                if (id == null)
+                    return null;
+                var db = new WebFinalContext();
+                var c1 = db.Students.Where(x => x.Id == id).FirstOrDefault();
+                if (c1 != null)
+                {
+                    db.Students.Remove(c1);
+                    db.SaveChanges();
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        private object? insertStudent(Student s)
+        {
+            try
+            {
+                if (s == null)
+                    return null;
+                var db = new WebFinalContext();
+                var s1 = new Student();
+
+                if (s1.StudentEmail != s.StudentEmail)
+                    s1.StudentEmail = s.StudentEmail;
+                if (s1.StudentGender != s.StudentGender)
+                    s1.StudentGender = s.StudentGender;
+                if (s1.StudentTown != s.StudentTown)
+                    s1.StudentTown = s.StudentTown;
+                if (s1.StudentPhone != s.StudentPhone)
+                    s1.StudentPhone = s.StudentPhone;
+                if (s1.StudentName != s.StudentName)
+                    s1.StudentName = s.StudentName;
+                if (s1.StudentId != s.StudentId)
+                    s1.StudentId = s.StudentId;
+                db.Students.Add(s1);
+                db.SaveChanges();
+                return s1;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        private object? updateStudent(Student s)
+        {
+            try
+            {
+                if (s == null)
+                    return null;
+                var db = new WebFinalContext();
+                var s1 = db.Students.Where(x => x.Id == s.Id).FirstOrDefault();
+
+                if (s1.StudentEmail != s.StudentEmail)
+                    s1.StudentEmail = s.StudentEmail;
+                if (s1.StudentGender != s.StudentGender)
+                    s1.StudentGender = s.StudentGender;
+                if (s1.StudentTown != s.StudentTown)
+                    s1.StudentTown = s.StudentTown;
+                if (s1.StudentPhone != s.StudentPhone)
+                    s1.StudentPhone = s.StudentPhone;
+                if (s1.StudentName != s.StudentName)
+                    s1.StudentName = s.StudentName;
+                if (s1.StudentId != s.StudentId)
+                    s1.StudentId = s.StudentId;
+
+                db.Students.Update(s1);
+                db.SaveChanges();
+                return s1;
             }
             catch (Exception ex)
             {
--- WebFinal/wwwroot/js/home.js
+++ WebFinal/wwwroot/js/home.js
@@ -202,3 +202,202 @@ function deleteCourse(id) {
         });
     }
 }
+
+function openStudentModal(id) {
+    $("#btnSave").show();
+    $("btnInsert").hide();
+    if (lst != null && id != null && id > 0) {
+        var item = $.grep(lst, function (obj) {
+            return obj.id == id;
+        })[0];
+        $("#txtId").val(item.id);
+        $("#txtStuId").val(item.studentId);
+        $("#txtStuName").val(item.studentName);
+        $("#txtPhone").val(item.studentPhone);
+        $("#txtTown").val(item.studentTown);
+        $("#txtGender").val(item.studentGender);
+        $("#txtEmail").val(item.studentEmail);
+
+    }
+}
+function show() {
+    getStudent(1);
+}
+function getStudent(p) {
+    console.log(p);
+    $.ajax({
+        type: "POST",
+        url: "/Home/get_student",
+        data: { 'Page': p, 'Size': 5 },
+        async: false,
+        success: function (res) {
+            if (res.success) {
+                let data = res.data;
+
+                if (data.data != null && data.data != undefined) {
+                    let stt = (p - 1) * 5 + 1;
+                    let data1 = [];
+                    for (var i = 0; i < data.data.length; i++) {
+                        let item = data.data[i];
+                        item.STT = stt;
+                        data1.push(item);
+                        stt++;
+                    }
+                    lst = data1;
+                    $("#tblStudent tbody").html("");
+                    $("#studentTemplate").tmpl(data1).appendTo("#tblStudent tbody");
+                }
+
+                totalPage = data.totalPage;
+                $("#curSPage").text(p);
+
+            } else {
+                alert(res.message);
+            }
+        },
+        failure: function (res) {
+
+        },
+        error: function (res) {
+
+        }
+    });
+}
+function goPrevInStu() {
+    var curPage = parseInt($("#curSPage").text());
+    if (curPage == 1) {
+        alert("This is first page");
+    } else {
+        var p = curPage - 1;
+
+
+        getStudent(p)
+    }
+}
+function goNextInStu() {
+    var curPage = parseInt($("#curSPage").text());
+    if (curPage == totalPage) {
+        alert("This is last page");
+    } else {
+        var p = curPage + 1;
+
+        getStudent(p)
+    }
+}
+function deleteStudent(id) {
+    if (confirm("Are you sure delete this student?") == false)
+        return;
+    if (id != null && id != undefined && id > 0) {
+        $.ajax({
+            type: "POST",
+            url: "/Home/delete_student",
+            data: { 'id': id },
+            async: false,
+            success: function (res) {
+                if (res.success) {
+                    alert("Delete Success !!!");
+                    var page = parseInt($("#curSPage").text());
+                    getStudent(page);
+                } else {
+                    alert(res.message);
+                }
+            },
+            failure: function (res) {
+
+            },
+            error: function (res) {
+
+            }
+        });
+    }
+}
+function addNewStudent() {
+    $("#btnSave").hide();
+    $("btnInsert").show();
+    $("#txtId").val("");
+    $("#txtStuId").val("");
+    $("#txtStuName").val("");
+    $("#txtPhone").val();
+    $("#txtTown").val("");
+    $("#txtGender").val("");
+    $("#txtEmail").val("");
+}
+
+function saveStudent() {
+    var item = {
+        id: $("#txtId").val(),
+        studentId: $("#txtStuId").val(),
+        studentName: $("#txtStuName").val(),
+        studentPhone: $("#txtPhone").val(),
+        studentTown: $("#txtTown").val(),
+        studentGender: $("#txtGender").val(),
+        studentEmail: $("#txtEmail").val(),
+
+    };
+    console.log(item);
+    $.ajax({
+        type: "POST",
+        url: "/Home/update_student",
+        data: { 'Student': item },
+        async: false,
+        success: function (res) {
+            if (res.success) {
+                alert("Update Success !!!");
+                let c = res.data;
+                var i = 0
+                for (i = 0; i < lst.length; i++) {
+                    if (lst[i].id == c.id) {
+                        c.STT = lst[i].STT;
+                        break;
+                    }
+                }
+                lst[i] = c;
+                $("#tblStudent tbody").html("");
+                $("#studentTemplate").tmpl(lst).appendTo("#tblStudent tbody");
+            } else {
+                alert(res.message);
+            }
+        },
+        failure: function (res) {
+
+        },
+        error: function (res) {
+
+        }
+    });
+}
+function insertStudent() {
+    var item = {
+        id: 0,
+        studentId: $("#txtStuId").val(),
+        studentName: $("#txtStuName").val(),
+        studentPhone: $("#txtPhone").val(),
+        studentTown: $("#txtTown").val(),
+        studentGender: $("#txtGender").val(),
+        studentEmail: $("#txtEmail").val(),
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/Home/insert_student",
+        data: { 'Student': item },
+        async: false,
+        success: function (res) {
+            if (res.success) {
+                alert("Insert Success !!!");
+                let c = res.data;
+                $("#txtId").val(c.id);
+
+                getStudent(1);
+            } else {
+                alert(res.message);
+            }
+        },
+        failure: function (res) {
+
+        },
+        error: function (res) {
+
+        }
+    });
+}
